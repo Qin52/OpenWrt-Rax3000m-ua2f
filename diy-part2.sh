e7da182c9cc5f08fb8482ca7db8823f9c32314c1
Qin 52 <61403052+Qin52@users.noreply.github.com>
2024-11-17 16:32:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,14 +24,14 @@ sed -i 's/OpenWrt/RAX3000m/g' package/base-files/files/bin/config_generate
 #sed -i 's/WHK_Telecom/OpenWrt5G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
 #加入UA2F模块和RKP-IPID模块以及nettask模块
-git clone https://github.com/EOYOHOO/UA2F.git package/UA2F
+git clone https://github.com/Zxilly/UA2F.git package/UA2F
 git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
 #git clone https://github.com/lucikap/luci-app-nettask.git package/nettask
 
 #添加额外软件包
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
-git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
+#git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 #git clone https://github.com/sirpdboy/luci-app-poweroffdevice.git package/luci-app-poweroffdevice
 
 #修改闪存为256M版本(这是针对原厂128闪存来的，但又要编译256M固件来的）
