1034d7ecc8271278d2ab1eaad163ff20039f61d9
2522G大本营 <61403052+Qin52@users.noreply.github.com>
2025-11-14 10:06:23 +0800

    删除passwall2

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,8 +11,8 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.6.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.6.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
 sed -i 's/LEDE/Tr3000/g' package/base-files/files/bin/config_generate
